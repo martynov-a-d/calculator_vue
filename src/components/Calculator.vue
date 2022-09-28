12b7e44a31d5f16cd59d4f4d284519fa7f3ec359
Alex Martynov <a.martynov@216base.ru>
2022-09-28 13:51:12 +0300

    add checkbox for show keyboard 0-9

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="calculator">
     <h1>{{ msg }}</h1>
-    <input type="text" v-model.number="operand1">
-    <h2>{{ result }}</h2>
-    <input type="text" v-model.number="operand2">
-    <button v-on:click="summ">+</button>
-    <button v-on:click="minus">-</button>
-    <button v-on:click="multiply">*</button>
-    <button v-on:click="division">/</button>
-    <button v-on:click="exponentiation">x^</button>
-    <button v-on:click="integer">%</button>
+    <div>{{test ? "Введите первое число" : "Введите второе число число"}}</div>
+    <input 
+      type="text" 
+      v-model.number="operand1.value" 
+      v-on:click="activeCheckbox" 
+      value="">
+    <input 
+      type="text" 
+      v-model.number="operand2.value"
+      v-on:click="activeCheckbox">
+    <h2 v-bind:class="{'error' : error}">{{ error ? "ERROR" : result }}</h2>
+    <input 
+      type="checkbox" 
+      id="operands_show"
+      v-model="isHidden">
+    <label for="operands_show">Показать клавиатуру</label>
+    <div class="operands" v-show="isHidden">
+      <button class="operand-elem"
+        v-for="elem in operands" 
+        v-bind:key="elem.id"
+        v-on:click="operand1.value = elem.name">
+        {{ elem.name }}
+      </button>
+    </div>
+    <div class="operations">
+      <button class="operation-elem"
+        v-for="elem in operations" 
+        v-bind:key="elem.id" 
+        v-on:click="methodHandler(elem.id)">
+        {{ elem.name.toUpperCase() }}
+      </button>
+    </div>
   </div>
 </template>
 
@@ -21,48 +44,127 @@ export default {
   },
   data() {
      return {
+      isHidden: true,
+      test: false,
+      error: false,
       result: 0,
-      operand1: 0,
-      operand2: 0,
+      operand1: {
+        name: "Первый",
+        flag: true,
+        value: 0,
+      },
+      operand2: {
+        name: "Второй",
+        flag: false,
+        value: 0,
+      },
+      operations: [
+        {
+          name: 'summ',
+          id: '+'
+        },
+        {
+          name: 'minus',
+          id: '-'
+        },
+        {
+          name: 'multyply',
+          id: '*'
+        },
+        {
+          name: 'divide',
+          id: '/'
+        },
+        {
+          name: 'exponentiation',
+          id: '**'
+        },
+        {
+          name: 'modulo',
+          id: '%'
+        },
+      ],
+      operands: [
+        {
+          name: 0,
+          id: 0,
+        },
+        {
+          name: 1,
+          id: 1,
+        },
+        {
+          name: 2,
+          id: 2,
+        },
+        {
+          name: 3,
+          id: 3,
+        },
+        {
+          name: 4,
+          id: 4,
+        },
+        {
+          name: 5,
+          id: 5,
+        },
+        {
+          name: 6,
+          id: 6,
+        },
+        {
+          name: 7,
+          id: 7,
+        },
+        {
+          name: 8,
+          id: 8,
+        },
+        {
+          name: 9,
+          id: 9,
+        },
+      ]
      }
   },
   methods: {
-    summ() {
-      this.result = this.operand1 + this.operand2
+    activeCheckbox(e) {
+      console.log(e)
     },
-    minus() {
-      this.result = this.operand1 - this.operand2
+    methodHandler(key) {
+      this.error = false;
+      switch (key) {
+        case '+':
+          this.result = this.operand1.value + this.operand2.value
+          break;
+        case '-':
+          this.result = this.operand1.value - this.operand2.value
+          break;
+        case '*':
+          this.result = this.operand1.value * this.operand2.value
+          break;
+        case '/':
+          if(this.operand2.value !== 0) {
+            this.result = this.operand1.value / this.operand2.value
+          } else {this.error = true}
+          break;
+        case '**':
+          this.result = this.operand1.value ** this.operand2.value
+          break;
+        case '%':
+          this.result = this.operand1.value % this.operand2.value
+          break;
+
+        default:
+          break;
+      }
     },
-    multiply() {
-      this.result = this.operand1 * this.operand2
-    },
-    division() {
-      this.result = this.operand1 / this.operand2
-    },
-    exponentiation() {
-      this.result = this.operand1 ** this.operand2
-    },
-    integer() {
-      this.result = this.operand1 % this.operand2
-    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+@import '../assets/Calculator.scss';
 </style>
